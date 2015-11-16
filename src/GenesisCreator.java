46baff3249202562f90fe9c4bd83be9861bda59b
Luis Schubert <schubert.luis@me.com>
2015-11-15 21:44:52 -0800

    created first mock genesis block

--- src/GenesisCreator.java
+++ src/GenesisCreator.java
@@ -1,5 +1,26 @@
+import java.io.FileOutputStream;
+
 /**
  * Created by lschubert on 11/15/15.
  */
 public class GenesisCreator {
+    public static void main(String[] args)throws Exception{
+        byte[] genBlockByteView = {0x4A, 0x5B};
+
+        FileOutputStream fos = new FileOutputStream("genesisBlock.b");
+        fos.write(genBlockByteView);
+        fos.close();
+
+    }
+
+    public static void printHex(byte[] ba) {
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+        for (int i=0;i<ba.length;i++) {
+            String hex=Integer.toHexString(0xff & ba[i]);
+            if(hex.length()==1) hexString.append('0');
+            hexString.append(hex);
+        }
+        System.out.println(hexString.toString());
+    }
 }
